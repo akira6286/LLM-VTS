d117613c6569ef99a119cb594995bd0da2f278c5
akira6286 <41689706+akira6286@users.noreply.github.com>
2025-07-17 12:19:02 +0800

    Delete speak.py

--- speak.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-def rin_speak(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 160)
-    engine.setProperty('voice', 'female')
-    engine.say(text)
-    engine.runAndWait()
